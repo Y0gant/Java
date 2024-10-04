7a9287caf9a4e8af98dc6c9913f6f28afc47ab99
Y0gant <yogantfaye731@proton.me>
2024-10-04 17:11:56 +0530

    while and do while added to controlstatements2

--- COR/ControlStatements2.java
+++ COR/ControlStatements2.java
@@ -10,9 +10,9 @@ public class ControlStatements2 {
 		int sum = 0;
 
 		// for loop begins
-		// and runs till x <= 20
-		for (int x = 1; x <= 20; x++) {
-			sum = sum + x;
+		// and runs till y <= 20
+		for (int y = 1; y <= 20; y++) {
+			sum = sum + y;
          }
          //then prints value of sum 
         System.out.println("Sum: " + sum);
@@ -31,8 +31,55 @@ public class ControlStatements2 {
             // this act as s break that add a line for previous loop
 			System.out.println(); 
 		}
-	}
+
+		// while loop
+//initialization of a variable as counter
+		int w =1;
+//conditional statement in while loop 
+		while (w<6) { 
+			System.out.println("hello world");
+		// updation expression that increments the counter variable
+			w++;
+
+			}
+			
+
+			int y = 1;
+ sum=0;
+			// Exit when y becomes greater than 10
+			
+		
+	while (y <= 10) {
+				// summing up y
+				sum = sum + y;
+				
+				// Increment the value of y for
+				// next iteration
+				y++;
+		}
+		System.out.println("Summation: " + sum);
+y=1;
+
+
+// do while loop
+do {
+	//statement
+	System.out.println(y);
+	//updation
+	y++;
 }
+//condition
+while(y<=10);
 
-
+// 2nd do while loop
+int x = 1; sum=0;
+System.out.println("2nd do while");
+do {
+	x = sum+x;
+	sum++;
 	
+	System.out.println(x);
+} while (sum<=20);
+
+}
+}
