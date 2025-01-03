6d94e52a489f18eec7c8126a619fff7194148123
Y0gant <yogantfaye731@proton.me>
2025-01-04 02:00:02 +0530

    Documentation

--- core/src/array/ArrayOfObject2.java
+++ core/src/array/ArrayOfObject2.java
@@ -6,16 +6,18 @@ package src.array;
  * an array of that object to store the parameters
  */
 public class ArrayOfObject2 {
+    // instance variables to store parameters of the method we will create
     int rollno;
     String name;
 
     public static void main(String[] args) {
         // created a reference variable to store array of objects
         ArrayOfObject2[] obj1 = new ArrayOfObject2[5];
+        //for loop to create object for each element of the array obj1
         for (int i = 0; i < obj1.length; i++) {
             obj1[i] = new ArrayOfObject2();
         }
-
+        //passing values to each object we created in the for loop
         obj1[0].Student(1, "Atharva");
         obj1[1].Student(2, "Deva");
         obj1[2].Student(3, "Gagan");
@@ -29,6 +31,7 @@ public class ArrayOfObject2 {
 
     }
 
+    //method to hold value of instance variables
     void Student(int rollno, String name) {
         this.rollno = rollno;
         this.name = name;
