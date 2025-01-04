f7484d853569aeee79a1b4db1238c989a3bfc7c8
Y0gant <yogantfaye731@proton.me>
2025-01-04 17:20:31 +0530

    passing array to method as parameter

---
+++ core/src/array/PassingArrayToMethod.java
@@ -0,0 +1,18 @@
+package src.array;
+
+public class PassingArrayToMethod {
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 2, 7, 2, 5};
+        sum(arr);
+    }
+
+    public static void sum(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        System.out.println(sum);
+    }
+
+}
