2d8f1f771693122b8f216acf868dee6c81a197da
Y0gant <yogantfaye731@proton.me>
2025-01-06 19:37:30 +0530

    Strings in java

---
+++ core/src/string_s/Str1.java
@@ -0,0 +1,12 @@
+package src.string_s;
+
+public class Str1 {
+    public static void main(String[] args) {
+        //Using String Literal
+        String str = "Java";
+        //Using new Keyword
+        //new object created in heap memory
+        // and the string literal is stored in string pool
+        String Str2 = new String("hello there");
+    }
+}
