2b66a62efe3b68237ac87a87fa51a70b83dc2d41
Y0gant <yogantfaye731@proton.me>
2025-01-08 18:52:03 +0530

    Getter and Setter method

---
+++ core/src/OOPS/GetSet.java
@@ -0,0 +1,24 @@
+package src.OOPS;
+
+public class GetSet {
+    int a;
+    int b;
+
+    public static void main(String[] args) {
+        GetSet Obj1 = new GetSet();
+        Obj1.set(12, 13);
+        System.out.println(Obj1.getSum());
+    }
+
+    //setter method for setting value
+    public void set(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    //Getter method for getting that value
+    public int getSum() {
+
+        return a + b;
+    }
+}
