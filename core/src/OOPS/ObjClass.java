2f576c7eb2365f88a077123069b9b4484724035a
Y0gant <yogantfaye731@proton.me>
2025-01-14 19:05:21 +0530

    Object class
    toString() & hashCode() method

---
+++ core/src/OOPS/ObjClass.java
@@ -0,0 +1,25 @@
+package src.OOPS;
+
+/**
+ * This program contains use of toString()
+ * & hashcode() method of object class.
+ */
+public class ObjClass {
+    String n;
+
+    ObjClass(String n) {
+        this.n = n;
+
+    }
+
+    public static void main(String[] args) {
+        ObjClass obj1 = new ObjClass("John Snow");
+        System.out.println(obj1);
+        System.out.println(obj1.hashCode());
+    }
+
+    @Override
+    public String toString() {
+        return "Name of Character :{" + n + "}";
+    }
+}
