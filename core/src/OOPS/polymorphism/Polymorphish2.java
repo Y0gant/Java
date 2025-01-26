3ae9c8d56ea8ce54cfa9d2dcfa04e34e91a0b85b
Y0gant <yogantfaye731@proton.me>
2025-01-26 18:08:13 +0530

    Polymorphism using method overriding

---
+++ core/src/OOPS/polymorphism/Polymorphish2.java
@@ -0,0 +1,34 @@
+package src.OOPS.polymorphism;
+
+//Polymorphism using method overriding
+class Person {
+    public void printDetails() {
+        System.out.println("In " + this.getClass().getSimpleName() + " class current object is a Person");
+    }
+}
+
+class Father extends Person {
+    @Override
+    public void printDetails() {
+        System.out.println("In " + this.getClass().getSimpleName() + " class current object is a Father");
+    }
+}
+
+class Friend extends Person {
+    @Override
+    public void printDetails() {
+        System.out.println("In " + this.getClass().getSimpleName() + " class current object is a Friend");
+    }
+}
+
+//driver class
+public class Polymorphish2 {
+    public static void main(String[] args) {
+        Person obj = new Person();
+        Person obj2 = new Father();
+        Person obj3 = new Friend();
+        obj.printDetails();
+        obj2.printDetails();
+        obj3.printDetails();
+    }
+}
