4373e0009146f687d312debde3c2c4ac363ac606
Y0gant <yogantfaye731@proton.me>
2025-01-28 20:12:37 +0530

    Employee Management Program
    -Dynamic object creation added

--- core/src/OOPS/EmpManagement.java
+++ core/src/OOPS/EmpManagement.java
@@ -1,5 +1,7 @@
 package src.OOPS;
 
+import java.util.Scanner;
+
 //Base class
 abstract class Employee {
     //Data fields
@@ -131,15 +133,44 @@ class Intern extends Employee {
     @Override
     void printDetails() {
         super.printDetails();
-        System.out.println("Base Salary :" + getSalary());
+        // System.out.println("Base Salary :" + getSalary());
         System.out.println("Project Incentives :" + stipend);
-        System.out.println("Total Salary :" + calculateSalary());
+        System.out.println("Total Stipend :" + calculateSalary());
         System.out.println();
     }
 }
 
 //Driver class
 public class EmpManagement {
+
+    //Method to create a dynamic object using user input
+    //return type=object of the class.
+    public static Employee createEmployee(String role) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter details for " + role + " :");
+        System.out.println("Name :");
+        String name = scanner.nextLine();
+        System.out.println("ID :");
+        int id = scanner.nextInt();
+        System.out.println("Salary :");
+        int salary = scanner.nextInt();
+        if (role.equals("Manager")) {
+            System.out.println("Bonus :");
+            int bonus = scanner.nextInt();
+            return new Manager(name, id, salary, bonus);
+        } else if (role.equals("Developer")) {
+            System.out.println("Project Incentives :");
+            int incentives = scanner.nextInt();
+            return new Developer(name, id, salary, incentives);
+        } else if (role.equals("Intern")) {
+            return new Intern(name, id, salary);
+        } else {
+            System.out.println("Invalid Input");
+            return null;
+        }
+    }
+
+
     public static void main(String[] args) {
         Employee manager1 = new Manager("Alice", 1, 80000, 10000); // Manager 1
         Employee manager2 = new Manager("David", 4, 90000, 12000); // Manager 2
@@ -147,14 +178,21 @@ public class EmpManagement {
         Employee developer2 = new Developer("Eve", 5, 70000, 6000); // Developer 2
         Employee intern1 = new Intern("Charlie", 3, 15000); // Intern 1
         Employee intern2 = new Intern("Grace", 6, 20000); // Intern 2
+        //Creating user defined objects
+        Employee manager3 = createEmployee("Manager");
+        Employee developer3 = createEmployee("Developer");
+        Employee intern3 = createEmployee("Intern");
 
         // Printing details of all employees
         manager1.printDetails();
         manager2.printDetails();
+        manager3.printDetails();
         developer1.printDetails();
         developer2.printDetails();
+        developer3.printDetails();
         intern1.printDetails();
         intern2.printDetails();
+        intern3.printDetails();
 
         // Testing Overloaded printDetails
         System.out.println("Testing overloaded printDetails method:");
