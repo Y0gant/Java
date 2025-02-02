924ce03d513ca93243c3871dbc098cc85f030e52
Y0gant <yogantfaye731@proton.me>
2025-02-03 04:46:09 +0530

    Enums usage with constructor field and method

---
+++ core/src/OOPS/Enums3.java
@@ -0,0 +1,21 @@
+package src.OOPS;
+
+enum Colors {
+    Red(10), Green(20), Blue(30), Yellow(40), Violet(50), Black(60);
+    int n;
+
+    Colors(int n) {
+        this.n = n;
+    }
+
+    public int getValue() {
+        return n;
+    }
+}
+
+public class Enums3 {
+    public static void main(String[] args) {
+        Colors[] cc = Colors.values();
+        for (Colors n : cc) System.out.println(n + " : " + n.getValue());
+    }
+}
