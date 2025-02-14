d20d627e319c3ff997c9b6512f45532b4487184e
Y0gant <yogantfaye731@proton.me>
2025-02-15 03:54:13 +0530

    Concurrent linked Queue demo

---
+++ src/main/java/core/collectionframework/queue_deque/ConcurrentLinkedQueueDemo.java
@@ -0,0 +1,37 @@
+package core.collectionframework.queue_deque;
+
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class ConcurrentLinkedQueueDemo {
+    private static ConcurrentLinkedQueue<String> taskQueue = new ConcurrentLinkedQueue<>();
+
+    public static void main(String[] args) {
+
+        Thread producer = new Thread(() -> {
+            for (int i = 0; i < 20000; i++) {
+
+                try {
+                    taskQueue.add("Task " + System.currentTimeMillis());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Thread consumer = new Thread(() -> {
+            for (int i = 0; i < 20000; i++) {
+
+                try {
+                    String task = taskQueue.poll();
+                    System.out.println("Processing: " + task);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        producer.start();
+        consumer.start();
+
+
+    }
+}
