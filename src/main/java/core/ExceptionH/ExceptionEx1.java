ed54dd9a8fdc77a56d260b1c9f9a246e37a43a49
Y0gant <yogantfaye731@proton.me>
2025-02-17 04:13:49 +0530

    Exception handling with try-catch

---
+++ src/main/java/core/ExceptionH/ExceptionEx1.java
@@ -0,0 +1,20 @@
+package core.ExceptionH;
+
+public class ExceptionEx1 {
+    public static void main(String[] args) {
+        int[] num = {10, 20, 30, 40, 50};
+        int[] den = {2, 3, 0, 6, 5};
+
+        for (int i = 0; i < num.length; i++) {
+            try {
+                System.out.println(divideNumbers(num[i], den[i]));
+            } catch (ArithmeticException e) {
+                System.out.println("Arithmetic Exception : cannot divide by zero at index " + i);
+            }
+        }
+    }
+
+    public static int divideNumbers(int a, int b) {
+        return a / b;
+    }
+}
