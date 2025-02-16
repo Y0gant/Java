9d611b133e9cf8f61b6a6b2315c409596bfac81f
Y0gant <yogantfaye731@proton.me>
2025-02-17 04:36:53 +0530

    Exception handling with try-catch-finally

---
+++ src/main/java/core/ExceptionH/ExceptionEx2.java
@@ -0,0 +1,23 @@
+package core.ExceptionH;
+
+import java.util.Scanner;
+
+public class ExceptionEx2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number :");
+        int n = sc.nextInt();
+        try {
+            System.out.println(divideHundred(n));
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero :");
+        } finally {
+            sc.close();
+        }
+      
+    }
+
+    public static int divideHundred(int n) {
+        return 100 / n;
+    }
+}
