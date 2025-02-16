97f7aba5ab6ff044dae7a54a891397c1600dccfc
Y0gant <yogantfaye731@proton.me>
2025-02-17 05:16:19 +0530

    Exception handling

---
+++ src/main/java/core/ExceptionH/NullPointerExceptionExample.java
@@ -0,0 +1,20 @@
+package core.ExceptionH;
+
+public class NullPointerExceptionExample {
+    public static void main(String[] args) {
+        String str = null; // Simulating a null value
+
+        try {
+            printStringLength(str);
+        } catch (NullPointerException e) {
+            System.out.println("Exception caught: String is null.");
+        }
+    }
+
+    public static void printStringLength(String str) {
+        if (str == null) {
+            throw new NullPointerException("String is null");
+        }
+        System.out.println("Length of the string: " + str.length());
+    }
+}
