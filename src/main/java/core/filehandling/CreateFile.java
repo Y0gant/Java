7861af25d8a5a8b7e9be8e6ac23130d28f21843a
Y0gant <yogantfaye731@proton.me>
2025-02-18 05:02:01 +0530

    File Handling-creating a file using user input

--- src/main/java/core/filehandling/CreateFile.java
+++ src/main/java/core/filehandling/CreateFile.java
@@ -2,11 +2,15 @@ package core.filehandling;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class CreateFile {
     public static void main(String[] args) {
         CreateFile file1 = new CreateFile();
-        File myFile = file1.createFile("pilot.txt");
+        System.out.println("Enter File name :");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        File myFile = file1.createFile(name);
         System.out.println("Returned file object :" + myFile.getAbsolutePath());
     }
 
