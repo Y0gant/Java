043ec1b458458876e0af78946a38a649f827044c
Y0gant <yogantfaye731@proton.me>
2025-02-19 01:54:11 +0530

    File Handling-reading a file

--- src/main/java/core/filehandling/ReadFile.java
+++ src/main/java/core/filehandling/ReadFile.java
@@ -1,5 +1,29 @@
 package core.filehandling;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class ReadFile {
-    
+    public static void main(String[] args) {
+        ReadFile file1 = new ReadFile();
+        CreateFile file2 = new CreateFile();
+        WriteFile file3 = new WriteFile();
+        File read = file2.createFile();
+        file3.writeToFile(read);
+        file1.readFile(read);
+    }
+
+    public void readFile(File toRead) {
+        try {
+            Scanner reader = new Scanner(toRead);
+            while (reader.hasNext()) {
+                String data = reader.nextLine();
+                System.out.println(data);
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!!");
+        }
+    }
 }
