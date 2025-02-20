e4131ffd4e7e391adaf0bf7d8e393b2899e892ae
Y0gant <yogantfaye731@proton.me>
2025-02-21 05:21:31 +0530

    FileInputStream Example

---
+++ src/main/java/core/java_io/FileInputStreamExample.java
@@ -0,0 +1,33 @@
+package core.java_io;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileInputStreamExample {
+
+    public static void main(String[] args) {
+        try (FileInputStream fi = new FileInputStream("Panigale.txt")) {
+
+            System.out.println("Channel :" + fi.getChannel());
+
+            System.out.println("File description :" + fi.getFD());
+
+            fi.skip(4);
+
+            System.out.println("File contents :");
+
+            int ch;
+            while ((ch = fi.read()) != -1) {
+                System.out.print((char) ch);
+            }
+            System.out.println();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: Ensure 'file1.txt' exists in the working directory. " + e.getLocalizedMessage());
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the file: " + e.getMessage());
+        }
+    }
+
+}
