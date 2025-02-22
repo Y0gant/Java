a4d2e385f0d68b9b86e5d91b00089b75f247599f
Y0gant <yogantfaye731@proton.me>
2025-02-23 02:23:06 +0530

    custom reader using DataInputStream -Fastest but more complex -not recommended

---
+++ src/main/java/core/java_io/ComplexReader.java
@@ -0,0 +1,58 @@
+package core.java_io;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+
+public class ComplexReader {
+
+
+    public static void main(String[] args) throws IOException {
+        Reader s = new Reader();
+        int n = s.nextInt();
+        int k = s.nextInt();
+        int count = 0;
+
+        while (n-- > 0) {
+            int x = s.nextInt();
+            if (x % k == 0) count++;
+        }
+        System.out.println(count);
+    }
+
+    static class Reader {
+        private final int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Reader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ') c = read();
+            boolean neg = (c == '-');
+            if (neg) c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            return neg ? -ret : ret;
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead) fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1) buffer[0] = -1;
+        }
+    }
+}
+
+
