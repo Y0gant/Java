5e8ecb2bdd85acd2054e91015733717fee991fcf
Y0gant <yogantfaye731@proton.me>
2025-02-28 13:56:58 +0530

    Volatile keyword Example

---
+++ src/main/java/core/synchronization/VolatileExample.java
@@ -0,0 +1,42 @@
+package core.synchronization;
+
+class SharedResource {
+    private volatile boolean flag = false;
+
+    public void setFlagTrue() {
+        flag = true;
+    }
+
+    public void printIfFlagTrue() {
+        while (!flag) {
+            try {
+                Thread.sleep(80);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            System.out.println("Flag is false");
+        }
+        System.out.println("Flag is now set to true by writerThread");
+    }
+}
+
+
+public class VolatileExample {
+    public static void main(String[] args) {
+        SharedResource obj = new SharedResource();
+
+        Thread writerThread = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            obj.setFlagTrue();
+        });
+
+        Thread readerThread = new Thread(obj::printIfFlagTrue);
+
+        writerThread.start();
+        readerThread.start();
+    }
+}
