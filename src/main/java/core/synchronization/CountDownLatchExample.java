e9edb4cbec2edf031b851c3ecf28c32d9c3af40e
Y0gant <yogantfaye731@proton.me>
2025-02-28 07:06:52 +0530

    CountDownLatch Example

---
+++ src/main/java/core/synchronization/CountDownLatchExample.java
@@ -0,0 +1,41 @@
+package core.synchronization;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+public class CountDownLatchExample {
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(3);
+        ExecutorService service = Executors.newFixedThreadPool(3);
+        service.submit(new DependentService(latch));
+        service.submit(new DependentService(latch));
+        service.submit(new DependentService(latch));
+        latch.await();
+        System.out.println("Main");
+        service.shutdown();
+    }
+
+}
+
+class DependentService implements Callable<String> {
+    private final CountDownLatch latch;
+
+    public DependentService(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    @Override
+    public String call() throws Exception {
+
+        try {
+            System.out.println(Thread.currentThread().getName() + " service started.");
+            Thread.sleep(2000);
+        } finally {
+            latch.countDown();
+        }
+        return "Ok";
+    }
+}
