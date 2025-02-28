04ac58b5f8c553c22554b4c8d077e3cf3cec4fbb
Y0gant <yogantfaye731@proton.me>
2025-02-28 14:16:32 +0530

    Atomic classes Example

---
+++ src/main/java/core/synchronization/AtomicExample.java
@@ -0,0 +1,38 @@
+package core.synchronization;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicExample {
+    private AtomicInteger counter = new AtomicInteger(0);
+
+    public static void main(String[] args) throws InterruptedException {
+        AtomicExample obj = new AtomicExample();
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 1; i <= 1000; i++) {
+                obj.incrementCounter();
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 1; i <= 1000; i++) {
+                obj.incrementCounter();
+            }
+        });
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println(obj.getCounter());
+
+    }
+
+    public int getCounter() {
+        return counter.get();
+    }
+
+    public void incrementCounter() {
+        counter.incrementAndGet();
+    }
+}
